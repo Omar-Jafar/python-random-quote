7723dd428512e249de2c4766e73571ecee8d6bce
Omar-Jafar <68290140+Omar-Jafar@users.noreply.github.com>
2020-07-16 23:11:46 +0200

    Give one or more a shot.

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
 import random
+
+
 def primary():
     f = open("quotes.txt")
     quotes = f.readlines()
     f.close()
     last = len(quotes) - 1
-    rnd = random.randint(0, last)
-    print(quotes[rnd])
+    for i in range(int(input())):
+        rnd = random.randint(0, last)
+        print(quotes[rnd])
 
 
 if __name__ == "__main__":
